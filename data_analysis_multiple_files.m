b349d22ff5f2a23430ec609340c99b86437aaee7
Andrea <a.giordano21@imperial.ac.uk>
2022-09-21 14:24:03 +0200

    files reading fixed

--- data_analysis_multiple_files.m
+++ data_analysis_multiple_files.m
@@ -11,7 +11,7 @@ set(groot,'DefaultLineLineWidth',1.5)
 
 MyFolderInfo = dir('usable Data');
 for j = 1:length(MyFolderInfo)
-    M(j, :, :) = readmatrix("usable Data/" + MyFolderInfo(1).name, "NumHeaderLines", 7, "Range", "A:I");
+    M(j, :, :) = readmatrix("usable Data/" + MyFolderInfo(j).name, "NumHeaderLines", 7, "Range", "A:I");
 end
 
 %% data correction
@@ -20,7 +20,7 @@ M_corrected(:, 1 : size(M, 2)-7, 1 : size(M, 3) - 1) = M(:, 8 : end, 1 : 9); % r
 
 %% data processing
 
-avg_F = mean(M_corrected(:, :, 4:6), 2); % average force vector for all of wing's config.
+avg_F = mean(M_corrected(1:end, :, 4:6), 2); % average force vector for all of wing's config.
 avg_T = mean(M_corrected(:, :, 7:9), 2); % average torque vector for all of wing's config. 
 
 %% data visualization
