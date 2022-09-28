47e652910e207038fbf26ee69544006239be3f9d
Andrea <a.giordano21@imperial.ac.uk>
2022-09-28 10:12:42 +0200

    fixed averaging and reading processes

--- data_analysis_multiple_files.m
+++ data_analysis_multiple_files.m
@@ -30,11 +30,11 @@ for k = 1:length(MyFolderInfo) % k = 1:40
     force_table = readtable("usable Data/" + MyFolderInfo(k).name, 'Delimiter', ', ', "Range", "D:F");
     torque_table = readtable("usable Data/" + MyFolderInfo(k).name, 'Delimiter', ', ', "Range", "G:I");   
 
-    avg_F(k, :) = mean(force_table{k, :});  % average force vector for all of wing's config.
-    avg_T(k, :) = mean(torque_table{k, :}); % average torque vector for all of wing's config.
+    avg_F(k, :) = mean(force_table{1:end, :});  % average force vector for all of wing's config.
+    avg_T(k, :) = mean(torque_table{1:end, :}); % average torque vector for all of wing's config.
 
-    std_F(k, :) = std(force_table{k, :});   % standard on for each force component of every wing config.
-    std_T(k, :) = std(torque_table{k, :});  % standard deviation for each torque component of every wing config.
+    std_F(k, :) = std(force_table{1:end, :});   % standard on for each force component of every wing config.
+    std_T(k, :) = std(torque_table{1:end, :});  % standard deviation for each torque component of every wing config.
 
 end
 
