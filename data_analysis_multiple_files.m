27d810f6cfdda8894412c64a2cd4431045cf7956
Andrea <a.giordano21@imperial.ac.uk>
2022-09-30 17:49:45 +0200

    WIP: improving data read by structure creation

--- data_analysis_multiple_files.m
+++ data_analysis_multiple_files.m
@@ -1,5 +1,5 @@
 clc
-clear all
+clear variables
 close all
 
 format long
@@ -11,11 +11,19 @@ set(groot,'DefaultLineLineWidth',1.5)
 
 MyFolderInfo = dir('usable Data');
 
-avg_F = zeros(length(MyFolderInfo), 3);
-avg_T = zeros(length(MyFolderInfo), 3);
+force = struct; torque = struct;    % definition of force and torque related quantities' containers
 
-std_F = zeros(length(MyFolderInfo), 3);
-std_T = zeros(length(MyFolderInfo), 3);
+force.avg = zeros(length(MyFolderInfo), 3);
+force.std = zeros(length(MyFolderInfo), 3);
+force.aoa = zeros(length(MyFolderInfo));
+force.vel = zeros(length(MyFolderInfo));
+force.inflation = zeros(length(MyFolderInfo));
+
+torque.avg = zeros(length(MyFolderInfo), 3);
+torque.std = zeros(length(MyFolderInfo), 3);
+torque.aoa = zeros(length(MyFolderInfo));
+torque.vel = zeros(length(MyFolderInfo));
+torque.inflation = zeros(length(MyFolderInfo));
 
 for k = 1:length(MyFolderInfo) 
 
@@ -30,17 +38,29 @@ for k = 1:length(MyFolderInfo)
     force_table = readtable("usable Data/" + MyFolderInfo(k).name, 'Delimiter', ', ', "Range", "D:F");
     torque_table = readtable("usable Data/" + MyFolderInfo(k).name, 'Delimiter', ', ', "Range", "G:I");   
 
-    avg_F(k, :) = mean(force_table{1:end, :});  % average force vector for all of wing's config.
-    avg_T(k, :) = mean(torque_table{1:end, :}); % average torque vector for all of wing's config.
+    avg_F(k, 1:3) = mean(force_table{1:end, :});  % average force vector for all of wing's config.
+    avg_T(k, 1:3) = mean(torque_table{1:end, :}); % average torque vector for all of wing's config.
 
     std_F(k, :) = std(force_table{1:end, :});   % standard on for each force component of every wing config.
     std_T(k, :) = std(torque_table{1:end, :});  % standard deviation for each torque component of every wing config.
+
+    if length(MyFolderInfo(k).name) == 11
+
+        avg_F(k, 4) = str2double(MyFolderInfo(k).name(1:2));
+        avg_T(k, 4) = str2double(MyFolderInfo(k).name(1:2));
+
+        avg_F(k, 5) = str2double(MyFolderInfo(k).name(1:2));
+        avg_T(k, 5) = str2double(MyFolderInfo(k).name(1:2));
+
+        avg_F(k, 6) = str2double(MyFolderInfo(k).name(1:2));
+        avg_T(k, 6) = str2double(MyFolderInfo(k).name(1:2));
+    end
 
 end
 
 %% data processing
 
-L = 1; 
+L = 1;
 
 % given a reference frame, Sf(x_s, y_s, z_s) represents the force sensing point,
 % whereas St(x_t, y_t, z_t) represents the torque sensing point.
@@ -68,6 +88,8 @@ d = [dx; dy; dz];
 tor_transposed = zeros(length(MyFolderInfo), 3);
 tor_transposed(1:end, 1:3) = avg_T(1:end, 1:3) + avg_F(1:end, 1:3) * d;
 
+%flow_speed = string_flow_speed(MyFolderInfo, length(MyFolderInfo));
+
 
 
 %% data visualization
@@ -80,5 +102,3 @@ tor_transposed(1:end, 1:3) = avg_T(1:end, 1:3) + avg_F(1:end, 1:3) * d;
 % plot(1:length(Fx), Fx, 'b');
 % 
 % plot(1:length(Fx), mean(Fx), '-r');
-
-
