19aed54c6c77ab23b18ad73768a763b81438be6e
Andrea <a.giordano21@imperial.ac.uk>
2022-10-05 10:15:24 +0200

    CL plot creation

--- data_analysis_multiple_files.m
+++ data_analysis_multiple_files.m
@@ -108,10 +108,16 @@ tor_transposed(1:end, 1:3) = torque.avg(1:end, 1:3) + force.avg(1:end, 1:3) * d;
 
 %% data visualization
 
+close all
+
+% plots against AoA
+
+
+
 sel_speed = [10, 15, 20, 30, 40, 50];
 sel_inflation = [0, 1, 2, 3, 4];
 
-% presenting one plot per selected speed and selected inflation, varying
+% CD / CL: presenting one plot per selected speed and selected inflation, varying
 % angle of attack
 
 % for j = 1:length(sel_speed)
@@ -136,10 +142,10 @@ sel_inflation = [0, 1, 2, 3, 4];
 %         hold off
 %     end
 % end
-% 
-close all
 
-% presenting one plot per selected speed and all inflations, varying
+
+
+% CD / CL: presenting one plot per selected speed and all inflations, varying
 % angle of attack
 
 for j = 1:length(sel_speed)
@@ -158,6 +164,7 @@ for j = 1:length(sel_speed)
     for k = 1:length(MyFolderInfo)
          if (force.vel(k) == sel_speed(j)) && (force.inflation(k) == sel_inflation(1))
             scatter(force.aoa(k), force.avg(k, 2) / force.avg(k, 1), 'r', 'filled');
+%             plot(force.aoa(k), force.avg(k, 2) / force.avg(k, 1), '-r');
          elseif (force.vel(k) == sel_speed(j)) && (force.inflation(k) == sel_inflation(2))
              scatter(force.aoa(k), force.avg(k, 2) / force.avg(k, 1), 'o', 'filled');
          elseif (force.vel(k) == sel_speed(j)) && (force.inflation(k) == sel_inflation(3))
@@ -176,6 +183,42 @@ for j = 1:length(sel_speed)
 end
 
 
+
+% CL: presenting one plot per selected speed and all inflations, varying
+% angle of attack
+
+for j = 1:length(sel_speed)
+    dyn_pressure = 0.5 * rho * sel_speed(j) ^ 2; % calculation of dynamic pressure
+
+    figure
+    title(['Flow Speed: ', num2str(sel_speed(j))])
+    hold on
+    grid on
+    xlabel('AoA')
+    ylabel('CL / CD')
+    xlim([-10 35])
+    ylim([-10 10])
+
+    for k = 1:length(MyFolderInfo)
+         if (force.vel(k) == sel_speed(j)) && (force.inflation(k) == sel_inflation(1))
+            scatter(force.aoa(k), force.avg(k, 2) / force.avg(k, 1), 'r', 'filled');
+%             plot(force.aoa(k), force.avg(k, 2) / force.avg(k, 1), '-r');
+         elseif (force.vel(k) == sel_speed(j)) && (force.inflation(k) == sel_inflation(2))
+             scatter(force.aoa(k), force.avg(k, 2) / force.avg(k, 1), 'o', 'filled');
+         elseif (force.vel(k) == sel_speed(j)) && (force.inflation(k) == sel_inflation(3))
+             scatter(force.aoa(k), force.avg(k, 2) / force.avg(k, 1), 'y', 'filled');
+         elseif (force.vel(k) == sel_speed(j)) && (force.inflation(k) == sel_inflation(4))
+             scatter(force.aoa(k), force.avg(k, 2) / force.avg(k, 1), 'b', 'filled');
+         elseif (force.vel(k) == sel_speed(j)) && (force.inflation(k) == sel_inflation(5))
+             scatter(force.aoa(k), force.avg(k, 2) / force.avg(k, 1), 'k', 'filled');
+         end
+    end
+    
+    legend({'inf. = 0 mL', 'inf. = 60 mL', 'inf. = 90 mL', 'inf. = 120 mL', 'inf. = 30 mL'}, ... 
+     'Location','northwest','Orientation','horizontal')
+    hold off
+
+end
 
 
 
