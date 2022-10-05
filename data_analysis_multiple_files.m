33738053125481865de58646dbeb99c0ad2b465c
Andrea <a.giordano21@imperial.ac.uk>
2022-10-05 16:08:17 +0200

    changed data reading structure in data read section and section title

--- data_analysis_multiple_files.m
+++ data_analysis_multiple_files.m
@@ -7,11 +7,20 @@ format long
 set(groot,'DefaultAxesFontSize',14)
 set(groot,'DefaultLineLineWidth',1.5)
 
-%% data read
+%% data reading
 
 MyFolderInfo = dir('usable Data');
 
 force = struct; torque = struct;    % definition of force and torque related quantities' containers
+exp_value  = struct;
+
+exp_value.f_avg = zeros(length(MyFolderInfo), 3);
+exp_value.f_std = zeros(length(MyFolderInfo), 3);
+exp_value.t_avg = zeros(length(MyFolderInfo), 3);
+exp_value.t_std = zeros(length(MyFolderInfo), 3);
+exp_value.aoa = zeros(length(MyFolderInfo), 1);
+exp_value.vel = zeros(length(MyFolderInfo), 1);
+exp_value.inflation = zeros(length(MyFolderInfo), 1);
 
 force.avg = zeros(length(MyFolderInfo), 3);
 force.std = zeros(length(MyFolderInfo), 3);
@@ -25,6 +34,83 @@ torque.aoa = zeros(length(MyFolderInfo), 1);
 torque.vel = zeros(length(MyFolderInfo), 1);
 torque.inflation = zeros(length(MyFolderInfo), 1);
 
+% for k = 1:length(MyFolderInfo) 
+% 
+%     if MyFolderInfo(k).name == '.' 
+%         continue
+%     end
+% 
+%     if MyFolderInfo(k).name == ".DS_Store" 
+%         continue
+%     end
+% 
+%     force_table = readtable("usable Data/" + MyFolderInfo(k).name, 'Delimiter', ', ', "Range", "D:F");
+%     torque_table = readtable("usable Data/" + MyFolderInfo(k).name, 'Delimiter', ', ', "Range", "G:I");   
+% 
+%     force.avg(k, :) = mean(force_table{1:end, :});  % average force vector for all of wing's config.
+%     torque.avg(k, 1:3) = mean(torque_table{1:end, :}); % average torque vector for all of wing's config.
+% 
+%     force.std(k, :) = std(force_table{1:end, :});   % standard on for each force component of every wing config.
+%     torque.std(k, :) = std(torque_table{1:end, :});  % standard deviation for each torque component of every wing config.
+% 
+%     if length(MyFolderInfo(k).name) == 11 || length(MyFolderInfo(k).name) > 12
+% 
+%         force.aoa(k) = str2double(MyFolderInfo(k).name(1:2));
+%         force.vel(k, 1) = str2double(MyFolderInfo(k).name(4:5));
+%         force.inflation(k) = str2double(MyFolderInfo(k).name(7));
+% 
+%         torque.aoa(k) = str2double(MyFolderInfo(k).name(1:2));
+%         torque.vel(k) = str2double(MyFolderInfo(k).name(4:5));
+%         torque.inflation(k) = str2double(MyFolderInfo(k).name(7));
+% 
+%     elseif length(MyFolderInfo(k).name) == 12
+% 
+%         force.aoa(k) = str2double(MyFolderInfo(k).name(2:3));
+%         force.vel(k, 1) = str2double(MyFolderInfo(k).name(5:6));
+%         force.inflation(k) = str2double(MyFolderInfo(k).name(8));
+% 
+%         torque.aoa(k) = str2double(MyFolderInfo(k).name(2:3));
+%         torque.vel(k) = str2double(MyFolderInfo(k).name(5:6));
+%         torque.inflation(k) = str2double(MyFolderInfo(k).name(8));
+% 
+%     end
+% 
+% end
+
+% for k = 1:length(MyFolderInfo) 
+% 
+%     if MyFolderInfo(k).name == '.' 
+%         continue
+%     end
+% 
+%     if MyFolderInfo(k).name == ".DS_Store" 
+%         continue
+%     end
+% 
+%     exp_table = readtable("usable Data/" + MyFolderInfo(k).name, 'Delimiter', ', ', "Range", "D:I");
+%     exp_value.f_avg(k, :) = mean(exp_table{1:end, 1:3});  % average force vector for all of wing's config.
+%     exp_value.f_std(k, :) = std(exp_table{1:end, 1:3});   % standard on for each force component of every wing config.
+%     exp_value.t_avg(k, :) = mean(exp_table{1:end, 4:6}); % average torque vector for all of wing's config.
+%     exp_value.t_std(k, :) = std(exp_table{1:end, 4:6});  % standard deviation for each torque component of every wing config.
+% 
+%     
+%     if length(MyFolderInfo(k).name) == 11 || length(MyFolderInfo(k).name) > 12
+% 
+%         exp_value.aoa(k) = str2double(MyFolderInfo(k).name(1:2));
+%         exp_value.vel(k, 1) = str2double(MyFolderInfo(k).name(4:5));
+%         exp_value.inflation(k) = str2double(MyFolderInfo(k).name(7));
+% 
+%     elseif length(MyFolderInfo(k).name) == 12
+% 
+%         exp_value.aoa(k) = str2double(MyFolderInfo(k).name(2:3));
+%         exp_value.vel(k, 1) = str2double(MyFolderInfo(k).name(5:6));
+%         exp_value.inflation(k) = str2double(MyFolderInfo(k).name(8));
+% 
+%     end
+% 
+% end
+
+
 for k = 1:length(MyFolderInfo) 
 
     if MyFolderInfo(k).name == '.' 
@@ -35,36 +121,17 @@ for k = 1:length(MyFolderInfo)
         continue
     end
 
-    force_table = readtable("usable Data/" + MyFolderInfo(k).name, 'Delimiter', ', ', "Range", "D:F");
-    torque_table = readtable("usable Data/" + MyFolderInfo(k).name, 'Delimiter', ', ', "Range", "G:I");   
+    exp_table = readtable("usable Data/" + MyFolderInfo(k).name, 'Delimiter', ', ', "Range", "D:I");
 
-    force.avg(k, :) = mean(force_table{1:end, :});  % average force vector for all of wing's config.
-    torque.avg(k, 1:3) = mean(torque_table{1:end, :}); % average torque vector for all of wing's config.
+    exp_value.f_avg(k, :) = mean(exp_table{1:end, 1:3});  % average force vector for all of wing's config.
+    exp_value.f_std(k, :) = std(exp_table{1:end, 1:3});   % standard on for each force component of every wing config.
+    
+    exp_value.t_avg(k, :) = mean(exp_table{1:end, 4:6}); % average torque vector for all of wing's config.
+    exp_value.t_std(k, :) = std(exp_table{1:end, 4:6});  % standard deviation for each torque component of every wing config.
 
-    force.std(k, :) = std(force_table{1:end, :});   % standard on for each force component of every wing config.
-    torque.std(k, :) = std(torque_table{1:end, :});  % standard deviation for each torque component of every wing config.
-
-    if length(MyFolderInfo(k).name) == 11 || length(MyFolderInfo(k).name) > 12
-
-        force.aoa(k) = str2double(MyFolderInfo(k).name(1:2));
-        force.vel(k, 1) = str2double(MyFolderInfo(k).name(4:5));
-        force.inflation(k) = str2double(MyFolderInfo(k).name(7));
-
-        torque.aoa(k) = str2double(MyFolderInfo(k).name(1:2));
-        torque.vel(k) = str2double(MyFolderInfo(k).name(4:5));
-        torque.inflation(k) = str2double(MyFolderInfo(k).name(7));
-
-    elseif length(MyFolderInfo(k).name) == 12
-
-        force.aoa(k) = str2double(MyFolderInfo(k).name(2:3));
-        force.vel(k, 1) = str2double(MyFolderInfo(k).name(5:6));
-        force.inflation(k) = str2double(MyFolderInfo(k).name(8));
-
-        torque.aoa(k) = str2double(MyFolderInfo(k).name(2:3));
-        torque.vel(k) = str2double(MyFolderInfo(k).name(5:6));
-        torque.inflation(k) = str2double(MyFolderInfo(k).name(8));
-
-    end
+    exp_value.aoa(k) = str2double(MyFolderInfo(k).name(1:2));
+    exp_value.vel(k, 1) = str2double(MyFolderInfo(k).name(4:5));
+    exp_value.inflation(k) = str2double(MyFolderInfo(k).name(7));
 
 end
 
