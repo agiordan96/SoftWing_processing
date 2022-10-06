fd5b2b937aa595a03eb6d9f5fbd2138593f76b9d
Andrea <a.giordano21@imperial.ac.uk>
2022-10-06 10:23:28 +0200

    skipping sumberged files

--- data_analysis_multiple_files.m
+++ data_analysis_multiple_files.m
@@ -44,6 +44,10 @@ for k = 1:length(MyFolderInfo)
         continue
     end
 
+    if length(MyFolderInfo(k).name) > 11    % skipping "submerged" files 
+        continue
+    end
+
     exp_table = readtable("usable Data full/" + MyFolderInfo(k).name, 'Delimiter', ', ', "Range", "D:I");
 
     exp_value.f_avg(k, :) = mean(exp_table{1:end, 1:3});  % average force vector for all of wing's config.
@@ -226,7 +230,12 @@ sel_inflation = [0, 1, 2, 3, 4];
 
 for j = 1:length(sel_speed)
     dyn_pressure = 0.5 * rho * sel_speed(j) ^ 2; % calculation of dynamic pressure
+    
     clear k1
+    clear k2
+    clear k3
+    clear k4
+    clear k5
 
     figure
     title(['CD plot # ', num2str(j), '; Flow Speed: ', num2str(sel_speed(j))])
@@ -234,15 +243,15 @@ for j = 1:length(sel_speed)
     grid on
     xlabel('AoA [deg]')
     ylabel('CL / CD')
-    xlim([-10 40])
-    ylim([-10 10])
+    xlim([-10 35])
+    ylim([-7 1])
 
     for k = 1:length(MyFolderInfo)
         if k == 87 
             continue
         end
          if (exp_value.vel(k) == sel_speed(j)) && (exp_value.inflation(k) == sel_inflation(1))
-            scatter(exp_value.aoa(k), exp_value.f_avg(k, 1), '*r');
+            scatter(exp_value.aoa(k), exp_value.f_avg(k, 1), '*r')
             if exist('k1','var') == 1
                 x_vec = [exp_value.aoa(k1), exp_value.aoa(k)];
                 y_vec = [exp_value.f_avg(k1, 1), exp_value.f_avg(k, 1)];
@@ -250,34 +259,34 @@ for j = 1:length(sel_speed)
             end
             k1 = k;
          elseif (exp_value.vel(k) == sel_speed(j)) && (exp_value.inflation(k) == sel_inflation(2))
-             scatter(exp_value.aoa(k), exp_value.f_avg(k, 1), '*o');
+             scatter(exp_value.aoa(k), exp_value.f_avg(k, 1), '*k')
              if exist('k2','var') == 1
-                x_vec = [exp_value.aoa(k1), exp_value.aoa(k)];
-                y_vec = [exp_value.f_avg(k1, 1), exp_value.f_avg(k, 1)];
-                plot(x_vec, y_vec, '--o')
+                x_vec = [exp_value.aoa(k2), exp_value.aoa(k)];
+                y_vec = [exp_value.f_avg(k2, 1), exp_value.f_avg(k, 1)];
+                plot(x_vec, y_vec, '--k')
             end
             k2 = k;
          elseif (exp_value.vel(k) == sel_speed(j)) && (exp_value.inflation(k) == sel_inflation(3))
-             scatter(exp_value.aoa(k), exp_value.f_avg(k, 1), '*m');
+             scatter(exp_value.aoa(k), exp_value.f_avg(k, 1), '*m')
              if exist('k3','var') == 1
-                x_vec = [exp_value.aoa(k1), exp_value.aoa(k)];
-                y_vec = [exp_value.f_avg(k1, 1), exp_value.f_avg(k, 1)];
+                x_vec = [exp_value.aoa(k3), exp_value.aoa(k)];
+                y_vec = [exp_value.f_avg(k3, 1), exp_value.f_avg(k, 1)];
                 plot(x_vec, y_vec, '--m')
             end
             k3 = k;
          elseif (exp_value.vel(k) == sel_speed(j)) && (exp_value.inflation(k) == sel_inflation(4))
-             scatter(exp_value.aoa(k), exp_value.f_avg(k, 1), '*b');
+             scatter(exp_value.aoa(k), exp_value.f_avg(k, 1), '*b')
              if exist('k4','var') == 1
-                x_vec = [exp_value.aoa(k1), exp_value.aoa(k)];
-                y_vec = [exp_value.f_avg(k1, 1), exp_value.f_avg(k, 1)];
+                x_vec = [exp_value.aoa(k4), exp_value.aoa(k)];
+                y_vec = [exp_value.f_avg(k4, 1), exp_value.f_avg(k, 1)];
                 plot(x_vec, y_vec, '--b')
             end
             k4 = k;
          elseif (exp_value.vel(k) == sel_speed(j)) && (exp_value.inflation(k) == sel_inflation(5))
-             scatter(exp_value.aoa(k), exp_value.f_avg(k, 1), '*g');
-             if exist('k5','var') == 1
-                x_vec = [exp_value.aoa(k1), exp_value.aoa(k)];
-                y_vec = [exp_value.f_avg(k1, 1), exp_value.f_avg(k, 1)];
+            scatter(exp_value.aoa(k), exp_value.f_avg(k, 1), '*g')
+            if exist('k5','var') == 1
+                x_vec = [exp_value.aoa(k5), exp_value.aoa(k)];
+                y_vec = [exp_value.f_avg(k5, 1), exp_value.f_avg(k, 1)];
                 plot(x_vec, y_vec, '--g')
             end
             k5 = k;
