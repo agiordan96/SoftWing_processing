a20b7d2fcc97fcf5c9c74c0d42f8279e45f9dfff
Andrea <a.giordano21@imperial.ac.uk>
2022-10-13 10:25:08 +0200

    legend, annotation and labels update

--- data_analysis_multiple_files.m
+++ data_analysis_multiple_files.m
@@ -238,7 +238,7 @@ for j = 1:length(sel_speed)
     hold on
     grid on
     xlabel('AoA [deg]','fontweight','bold','fontsize', 20);
-    ylabel('CL [E-06]','fontweight','bold','fontsize', 20);
+    ylabel('CL [ ]','fontweight','bold','fontsize', 20);
     xlim([-10 35])
     ylim([-.5 .5])
 
@@ -300,7 +300,7 @@ for j = 1:length(sel_speed)
          end
     end
     
-    str_annotation = sprintf('Re = %.3e', Re);
+    str_annotation = sprintf('Re = %.2e', Re);
     annotation('textbox', [0.696 0.77 0.1 0.1], 'String', str_annotation, ...
            'BackgroundColor','white','LineStyle','-','Fontsize', 16, 'Interpreter','latex' )
     hold off
@@ -337,7 +337,7 @@ for j = 1:length(sel_speed)
     hold on
     grid on
     xlabel('AoA [deg]','fontweight','bold','fontsize', 20);
-    ylabel('CD [E-06]','fontweight','bold','fontsize', 20);
+    ylabel('CD [ ]','fontweight','bold','fontsize', 20);
     xlim([-10 35])
 %     ylim([-7 1])
 
@@ -348,58 +348,58 @@ for j = 1:length(sel_speed)
 
          if (exp_value.vel(k) == sel_speed(j)) && (exp_value.inflation(k) == sel_inflation(1))
             if exist('k1','var') == 0
-                errorbar(exp_value.aoa(k), (exp_value.f_avg(k, 1) / div) * 10^(06), exp_value.f_std(k), 'or', 'DisplayName', 'inf. = 0 mL', 'CapSize', 18, 'MarkerFaceColor', 'r', 'LineWidth', 1, MarkerEdgeColor = 'red')
+                errorbar(exp_value.aoa(k), (exp_value.f_avg(k, 1) / div), exp_value.f_std(k), 'or', 'DisplayName', 'inf. = 0 mL', 'CapSize', 18, 'MarkerFaceColor', 'r', 'LineWidth', 1, MarkerEdgeColor = 'red')
             else
-                errorbar(exp_value.aoa(k), (exp_value.f_avg(k, 1) / div) * 10^(06), exp_value.f_std(k), 'or', 'HandleVisibility','off', 'CapSize', 18, 'MarkerFaceColor', 'r', 'LineWidth', 1, MarkerEdgeColor = 'red')
+                errorbar(exp_value.aoa(k), (exp_value.f_avg(k, 1) / div), exp_value.f_std(k), 'or', 'HandleVisibility','off', 'CapSize', 18, 'MarkerFaceColor', 'r', 'LineWidth', 1, MarkerEdgeColor = 'red')
                 x_vec = [exp_value.aoa(k1), exp_value.aoa(k)];
-                y_vec = [(exp_value.f_avg(k1, 1) / div) * 10^(06), (exp_value.f_avg(k, 1) / div) * 10^(06)];
+                y_vec = [(exp_value.f_avg(k1, 1) / div), (exp_value.f_avg(k, 1) / div)];
                 plot(x_vec, y_vec, '--r', 'HandleVisibility','off')
             end
             k1 = k;
          elseif (exp_value.vel(k) == sel_speed(j)) && (exp_value.inflation(k) == sel_inflation(2))
              if exist('k2','var') == 0
-                errorbar(exp_value.aoa(k), (exp_value.f_avg(k, 1) / div) * 10^(06), exp_value.f_std(k), 'ok', 'DisplayName', 'inf. = 30 mL', 'CapSize', 18, 'MarkerFaceColor', 'k', 'LineWidth', 1, MarkerEdgeColor = 'black')
+                errorbar(exp_value.aoa(k), (exp_value.f_avg(k, 1) / div), exp_value.f_std(k), 'ok', 'DisplayName', 'inf. = 30 mL', 'CapSize', 18, 'MarkerFaceColor', 'k', 'LineWidth', 1, MarkerEdgeColor = 'black')
              else
-                errorbar(exp_value.aoa(k), (exp_value.f_avg(k, 1) / div) * 10^(06), exp_value.f_std(k), 'ok', 'HandleVisibility','off', 'CapSize', 18, 'MarkerFaceColor', 'k', 'LineWidth', 1, MarkerEdgeColor = 'black')
+                errorbar(exp_value.aoa(k), (exp_value.f_avg(k, 1) / div), exp_value.f_std(k), 'ok', 'HandleVisibility','off', 'CapSize', 18, 'MarkerFaceColor', 'k', 'LineWidth', 1, MarkerEdgeColor = 'black')
                 x_vec = [exp_value.aoa(k2), exp_value.aoa(k)];
-                y_vec = [(exp_value.f_avg(k2, 1)  / div) * 10^(06), (exp_value.f_avg(k, 1) / div) * 10^(06)];
+                y_vec = [(exp_value.f_avg(k2, 1)  / div), (exp_value.f_avg(k, 1) / div)];
                 plot(x_vec, y_vec, '--k', 'HandleVisibility','off')
             end
             k2 = k;
          elseif (exp_value.vel(k) == sel_speed(j)) && (exp_value.inflation(k) == sel_inflation(3))
              if exist('k3','var') == 0
-                errorbar(exp_value.aoa(k), (exp_value.f_avg(k, 1) / div) * 10^(06), exp_value.f_std(k), 'om', 'DisplayName', 'inf. = 60 mL', 'CapSize', 18, 'MarkerFaceColor', 'm', 'LineWidth', 1, MarkerEdgeColor = 'magenta')
+                errorbar(exp_value.aoa(k), (exp_value.f_avg(k, 1) / div), exp_value.f_std(k), 'om', 'DisplayName', 'inf. = 60 mL', 'CapSize', 18, 'MarkerFaceColor', 'm', 'LineWidth', 1, MarkerEdgeColor = 'magenta')
              else
-                errorbar(exp_value.aoa(k), (exp_value.f_avg(k, 1) / div) * 10^(06), exp_value.f_std(k), 'om', 'HandleVisibility','off', 'CapSize', 18, 'MarkerFaceColor', 'm', 'LineWidth', 1, MarkerEdgeColor = 'magenta')
+                errorbar(exp_value.aoa(k), (exp_value.f_avg(k, 1) / div), exp_value.f_std(k), 'om', 'HandleVisibility','off', 'CapSize', 18, 'MarkerFaceColor', 'm', 'LineWidth', 1, MarkerEdgeColor = 'magenta')
                 x_vec = [exp_value.aoa(k3), exp_value.aoa(k)];
-                y_vec = [(exp_value.f_avg(k3, 1) / div) * 10^(06), (exp_value.f_avg(k, 1) / div) * 10^(06)];
+                y_vec = [(exp_value.f_avg(k3, 1) / div), (exp_value.f_avg(k, 1) / div)];
                 plot(x_vec, y_vec, '--m', 'HandleVisibility','off')
             end
             k3 = k;
          elseif (exp_value.vel(k) == sel_speed(j)) && (exp_value.inflation(k) == sel_inflation(4))
              if exist('k4','var') == 0
-                errorbar(exp_value.aoa(k), (exp_value.f_avg(k, 1) / div) * 10^(06), exp_value.f_std(k), 'ob', 'DisplayName', 'inf. = 90 mL', 'CapSize', 18, 'MarkerFaceColor', 'b', 'LineWidth', 1, MarkerEdgeColor = 'blue')
+                errorbar(exp_value.aoa(k), (exp_value.f_avg(k, 1) / div), exp_value.f_std(k), 'ob', 'DisplayName', 'inf. = 90 mL', 'CapSize', 18, 'MarkerFaceColor', 'b', 'LineWidth', 1, MarkerEdgeColor = 'blue')
              else
-                errorbar(exp_value.aoa(k), (exp_value.f_avg(k, 1) / div) * 10^(06), exp_value.f_std(k), 'ob', 'HandleVisibility','off', 'CapSize', 18, 'MarkerFaceColor', 'b', 'LineWidth', 1, MarkerEdgeColor = 'blue')
+                errorbar(exp_value.aoa(k), (exp_value.f_avg(k, 1) / div), exp_value.f_std(k), 'ob', 'HandleVisibility','off', 'CapSize', 18, 'MarkerFaceColor', 'b', 'LineWidth', 1, MarkerEdgeColor = 'blue')
                 x_vec = [exp_value.aoa(k4), exp_value.aoa(k)];
-                y_vec = [(exp_value.f_avg(k4, 1) / div) * 10^(06), (exp_value.f_avg(k, 1) / div) * 10^(06)];
+                y_vec = [(exp_value.f_avg(k4, 1) / div), (exp_value.f_avg(k, 1) / div)];
                 plot(x_vec, y_vec, '--b', 'HandleVisibility','off')
             end
             k4 = k;
          elseif (exp_value.vel(k) == sel_speed(j)) && (exp_value.inflation(k) == sel_inflation(5))
              if exist('k5','var') == 0
-                errorbar(exp_value.aoa(k), (exp_value.f_avg(k, 1) / div) * 10^(06), exp_value.f_std(k), 'og', 'DisplayName', 'inf. = 120 mL', 'CapSize', 18, 'MarkerFaceColor', 'g', 'LineWidth', 1, MarkerEdgeColor = 'green')
+                errorbar(exp_value.aoa(k), (exp_value.f_avg(k, 1) / div), exp_value.f_std(k), 'og', 'DisplayName', 'inf. = 120 mL', 'CapSize', 18, 'MarkerFaceColor', 'g', 'LineWidth', 1, MarkerEdgeColor = 'green')
              else
-                errorbar(exp_value.aoa(k), (exp_value.f_avg(k, 1) / div) * 10^(06), exp_value.f_std(k), 'og', 'HandleVisibility','off', 'CapSize', 18, 'MarkerFaceColor', 'g', 'LineWidth', 1, MarkerEdgeColor = 'green')
+                errorbar(exp_value.aoa(k), (exp_value.f_avg(k, 1) / div), exp_value.f_std(k), 'og', 'HandleVisibility','off', 'CapSize', 18, 'MarkerFaceColor', 'g', 'LineWidth', 1, MarkerEdgeColor = 'green')
                 x_vec = [exp_value.aoa(k5), exp_value.aoa(k)];
-                y_vec = [(exp_value.f_avg(k5, 1) / div) * 10^(06), (exp_value.f_avg(k, 1) / div) * 10^(06)];
+                y_vec = [(exp_value.f_avg(k5, 1) / div), (exp_value.f_avg(k, 1) / div)];
                 plot(x_vec, y_vec, '--g', 'HandleVisibility','off')
             end
             k5 = k;
          end
     end
     
-    str_annotation = sprintf('Re = %.3e', Re);
+    str_annotation = sprintf('Re = %.2e', Re);
     annotation('textbox', [0.696 0.77 0.1 0.1], 'String', str_annotation, ...
            'BackgroundColor','white','LineStyle','-','Fontsize', 16, 'Interpreter','latex' )
     hold off
